142a911ce4c9674879fa0828768de43b7dd14b9f
K Aslan <keremaslan@outlook.com>
2018-11-25 22:36:28 +0800

    Configuration setup and addition of benchmarker.

--- src/main/java/problems/tsp/app/TspApp.java
+++ src/main/java/problems/tsp/app/TspApp.java
@@ -1,11 +1,12 @@
 package problems.tsp.app;
 
-import jdk.swing.interop.SwingInterOpUtils;
-import org.optaplanner.core.api.score.Score;
+
+import org.optaplanner.benchmark.api.PlannerBenchmark;
+import org.optaplanner.benchmark.api.PlannerBenchmarkFactory;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
-import org.optaplanner.core.api.score.buildin.simplebigdecimal.SimpleBigDecimalScore;
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
+
 import problems.tsp.domain.Assignment;
 import problems.tsp.domain.TspSolution;
 import problems.tsp.persistence.TspProblemGenerator;
@@ -22,15 +23,20 @@ public class TspApp {
         SolverFactory solverFactory = SolverFactory.createFromXmlFile( new File("src/main/resources/problems/tsp/solver/TspSolverConfig.xml"));
         Solver<TspSolution> solver = solverFactory.buildSolver();
 
+        PlannerBenchmarkFactory benchmarkFactory = PlannerBenchmarkFactory.createFromSolverFactory(solverFactory);
 
+
+        //Load Western Sahara Dataset
         TspSolution problem = TspProblemGenerator.createTspProblem(TspProblemGenerator.Dataset.WESTERN_SAHARA);
-
-        System.out.println("# of assignments created " + problem.getAssignments().size());
+        // TODO Load another TSP Dataset for benchmarking purposes
 
         TspSolution solution = solver.solve(problem);
         HardSoftScore score = solution.getScore();
 
+//        PlannerBenchmark plannerBenchmark = benchmarkFactory.buildPlannerBenchmark(solution, problem);
+//        plannerBenchmark.benchmark();
         System.out.println("HardScore is " + score.getHardScore() + " soft score is "+ score.getSoftScore());
+
         for(Assignment assignment: solution.getAssignments()){
             System.out.println(assignment);
         }
