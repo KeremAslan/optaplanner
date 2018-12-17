b96e61e900969b4a40f829e2a26b63d6aed6c331
Kerem Aslan <kerem.aslan@redmart.com>
2018-12-17 06:30:32 +0000

    Benchmarking experimentation

--- src/main/java/problems/tsp/package-info.java
+++ src/main/java/problems/tsp/package-info.java
@@ -1,5 +1,6 @@
 package problems.tsp;
 
 /**
- * This package models the tsp as an assignment problem as opposed to leveraging optaplanner's chaining feature.
+ * This package is a solver modeled to solve the tsp problem on datasets from TSBLIB. Further improvements will also solve it for
+ * the assignments for the coursera course Discrete Optimization.
  */
