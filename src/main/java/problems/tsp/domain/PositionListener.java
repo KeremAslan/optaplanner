24e4556bad3c7219e76ee1421ddd58a014be8d0f
K Aslan <keremaslan@outlook.com>
2018-12-28 23:33:57 +0800

    Inclusion of PositionListener to update shadow variable

---
+++ src/main/java/problems/tsp/domain/PositionListener.java
@@ -0,0 +1,49 @@
+package problems.tsp.domain;
+
+import org.optaplanner.core.impl.domain.variable.listener.VariableListener;
+import org.optaplanner.core.impl.score.director.ScoreDirector;
+
+
+public class PositionListener implements VariableListener<Visit> {
+
+    @Override
+    public void beforeEntityAdded(ScoreDirector scoreDirector, Visit visit) {
+
+    }
+
+    @Override
+    public void afterEntityAdded(ScoreDirector scoreDirector, Visit visit) {
+
+    }
+
+    @Override
+    public void beforeVariableChanged(ScoreDirector scoreDirector, Visit visit) {
+
+    }
+
+    @Override
+    public void afterVariableChanged(ScoreDirector scoreDirector, Visit visit) {
+
+    }
+
+    @Override
+    public void beforeEntityRemoved(ScoreDirector scoreDirector, Visit visit) {
+
+    }
+
+    @Override
+    public void afterEntityRemoved(ScoreDirector scoreDirector, Visit visit) {
+
+    }
+
+    private void updatePosition(ScoreDirector scoreDirector, Visit visit) {
+        TspSolution tspSolution = (TspSolution) scoreDirector.getWorkingSolution();
+        Domicile domicile = tspSolution.getDomicile();
+
+        Integer position = visit.getPosition();
+        if(position != null) {
+
+        }
+
+    }
+}
